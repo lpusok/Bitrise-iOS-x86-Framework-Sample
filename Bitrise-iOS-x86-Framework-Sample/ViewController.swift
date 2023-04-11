3adde371efa61c5b0d85af81b31ea902a532af0e
Olivér Falvai <ofalvai@gmail.com>
2023-04-11 09:28:52 +0200

    Initial Commit

---
+++ Bitrise-iOS-x86-Framework-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitrise-iOS-x86-Framework-Sample
+//
+//  Created by Olivér Falvai on 2023. 04. 11..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
